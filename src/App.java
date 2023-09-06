cb3106dd23d5c5e0d263ca24f7d42731332838ec
Mustafa Maher <mustafa.maher@elev.ga.ntig.se>
2023-09-06 10:09:14 +0200

    test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int tal1 = 2;
+        int tal2 = 40;
+        int summa_addition = tal1 + tal2;
+        int differens_subtraktion = tal1 - tal2;
+        
+        System.out.println("summan av talen är "+summa_addition);
+        System.out.println("differensen på de är "+differens_subtraktion);
+
+        int produkt = tal1*tal2;
+        int kvot = tal1/tal2;
+
+        System.out.println("produkten är "+produkt);
+        System.out.println("kvoten är "+kvot);
     }
+
 }
